be299a80e8eb616e27464d02314d7ba588b8d3cf
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-25 11:06:50 +0300

    Add an algorithm to check if a string is palindrome

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const newWord = word.toLowerCase()
+
+  // reverses the string
+  let reversedWord = newWord.split('').reverse().join('');
+
+  if (newWord === reversedWord) {
+    return true
+  } else {
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  define a variable to hold the passed string
+   split the string using split('') method
+  reverse of the string passed as an argument;
+  Join the array of characters into one string
+  Compare the passed string with the reversed string
+  Return true if strings matches
+  Return false if string does not match
 */
 
 /*
   Add written explanation of your solution here
+  to check if the string is palindrome 
+  * Split the string into an array
+  * Reverse the string using the reverse inbuilt method
+  * Join the strings to using join() to form one string
+  * compare the string that was passed as an arguement with the new string formed
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +43,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting: true');
+  console.log("=>", isPalindrome("a"));
+
+  console.log('Expecting: true');
+  console.log("=>", isPalindrome(""));
+
+  console.log('Expecting: true');
+  console.log("=>", isPalindrome("MOM"));
+
+  console.log('Expecting: true');
+  console.log("=>", isPalindrome("moM"));
 }
 
 module.exports = isPalindrome;
